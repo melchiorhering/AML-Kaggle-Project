9f5eebb864d364860e397aea4c76ea4ac20a9873
Stijn Hering <stijn@hering.nl>
2023-12-05 11:09:32 +0100

    added fastai

--- models.py
+++ models.py
@@ -30,7 +30,7 @@ class SimpleCNN(nn.Module):
 
 class MoreComplex(nn.Module):
     def __init__(self):
-        super(SimpleCNN, self).__init__()
+        super(MoreComplex, self).__init__()
         self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1)
         self.bn1 = nn.BatchNorm2d(32)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
@@ -53,4 +53,31 @@ class MoreComplex(nn.Module):
         x = F.relu(self.fc1(x))
         x = self.dropout(x)
         x = self.fc2(x)
+        return x
+
+
+class Net(nn.Module):
+    """Models a simple Convolutional Neural Network"""
+
+    def __init__(self):
+        """initialize the network"""
+        super(Net, self).__init__()
+        # 3 input image channel, 6 output channels,
+        # 5x5 square convolution kernel
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        # Max pooling over a (2, 2) window
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)  # 5x5 from image dimension
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        """the forward propagation algorithm"""
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
         return x
